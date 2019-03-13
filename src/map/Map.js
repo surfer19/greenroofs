91d5e09b7799bf458376f62dc63549e4eb8e16fe
Marián Mrva <marianmrva123@gmail.com>
2019-03-13 23:46:27 +0100

    add webpack config with assets

--- src/map/Map.js
+++ src/map/Map.js
@@ -2,7 +2,8 @@
 import React from 'react';
 import { Map as LeafletMap, TileLayer, ImageOverlay, LayersControl, Marker, Popup, FeatureGroup, Circle, latLngBounds }  from 'react-leaflet';
 import './Map.css';
-import heatmapImg from '../assets/images/heatmap.jpg'; 
+import heatmapImg from '../assets/images/heatmap.jpg';
+import BuildingsAll from '../assets/terrierObjects/green_roofs.json'
 
 // import 'leaflet/dist/leaflet.css';
 
@@ -20,10 +21,11 @@ class Map extends React.Component {
             <LeafletMap
                 center={center}
                 zoom={15}            
-                // attributionControl={true}
-                // zoomControl={true}
-                >                
-                <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'/>
+                attributionControl={true}
+                zoomControl={true}
+                >
+                <TileLayer url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'></TileLayer>
+                
                 {/* <Circle center={center} color="red" fillColor="none" radius={1000} /> */}                
 
                 <LayersControl position="topright">      
@@ -32,13 +34,14 @@ class Map extends React.Component {
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         />
-                    </LayersControl.BaseLayer> */}
+                    </LayersControl.BaseLayer> */}                    
                     
-                    <LayersControl.Overlay name="Show heatmap">                        
-                        <ImageOverlay   url={heatmapImg} 
-                                            bounds={[[49.91156 ,13.90904], [50.2512, 14.89781]]}                                        
-                                            opacity="0.5">
+                    <LayersControl.Overlay name="Show heatmap">
+                        <ImageOverlay url={heatmapImg} 
+                                        bounds={[[49.91156 ,13.90904], [50.2512, 14.89781]]}                                        
+                                        opacity="0.5">                            
                         </ImageOverlay>
+
                     </LayersControl.Overlay>
                     <LayersControl.Overlay name="Feature group">
                         <FeatureGroup color="purple">
